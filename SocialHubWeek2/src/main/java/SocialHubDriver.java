8fbe2f9176e9eb8e389cab0a38200726d9a860d6
Ethan McGill <37718257+elmcgill@users.noreply.github.com>
2021-07-16 14:31:59 -0500

    week two complete

--- SocialHubWeek2/src/main/java/SocialHubDriver.java
+++ SocialHubWeek2/src/main/java/SocialHubDriver.java
@@ -14,38 +14,14 @@ import com.example.services.UserService;
 
 public class SocialHubDriver {
 	
-	//private static UserService uServ = new UserService("users.txt");
-	//private static PostService pServ = new PostService("posts.txt");
+	private static UserDao uDao = new UserDaoDB();
+	private static PostDao pDao = new PostDaoDB();
+	private static UserService uServ = new UserService(uDao);
+	private static PostService pServ = new PostService(pDao);
 	
 	public static void main(String[] args) {
 		
-		UserDao uDao = new UserDaoDB();
-		PostDao pDao = new PostDaoDB();
-		UserService uServ = new UserService(uDao);
-		PostService pServ = new PostService(pDao);
 		
-		
-		//Once we create rick once, we comment this out so there are no duplicates
-		//User rick = uServ.signUp("Rick", "Sanchez", "rick@rickmail.com", "IloveMorty");
-		
-		
-		User rick = uServ.signIn("RickSanchez1472", "IloveMorty");
-		
-		//pServ.addPost(rick.getId(), rick.getId(), "Morty and I just went on a sick adventure");
-		
-		List<PostDisplay> pList = pServ.getAllPosts();
-		
-		for(int i=0; i<pList.size(); i++) {
-			System.out.println(pList.get(i).getUsername());
-			System.out.println(pList.get(i).getContent());
-			System.out.println();
-		}
-		
-		rick = pServ.loadUserPosts(rick);
-		
-		System.out.println(rick);
-		
-		/*
 		Scanner in = new Scanner(System.in);
 		
 		//This will be used to control our loop
@@ -63,7 +39,7 @@ public class SocialHubDriver {
 					System.out.print("Please enter your password: ");
 					String password = in.nextLine();
 					try {
-						u = uServ.login(username, password);
+						u = uServ.signIn(username, password);
 						System.out.println("Welcome " + u.getFirstName());
 					} catch(Exception e) {
 						System.out.println("Username or password was incorect. Goodbye");
@@ -72,12 +48,14 @@ public class SocialHubDriver {
 				} else {
 					System.out.print("Please enter you first name: ");
 					String first = in.nextLine();
-					System.out.println("Please enter your last name: ");
+					System.out.print("Please enter your last name: ");
 					String last = in.nextLine();
-					System.out.println("Please enter a password: ");
+					System.out.print("Please enter your email: ");
+					String email = in.nextLine();
+					System.out.print("Please enter a password: ");
 					String password = in.nextLine();
 					try {
-						u = uServ.signUp(first, last, password);
+						u = uServ.signUp(first, last, email, password);
 						System.out.println("You may now log in with the username: " + u.getUsername());
 					} catch (Exception e) {
 						System.out.println("Sorry, we could not process your request");
@@ -90,9 +68,9 @@ public class SocialHubDriver {
 				int choice = Integer.parseInt(in.nextLine());
 				//If the user chooses 1, we will show them the list of posts
 				if(choice == 1) {
-					List<Post> posts = pServ.getAllPosts();
-					for(Post post: posts) {
-						System.out.println(post.getUser() + ":");
+					List<PostDisplay> posts = pServ.getAllPosts();
+					for(PostDisplay post: posts) {
+						System.out.println(post.getUsername() + ":");
 						System.out.println(post.getContent());
 						System.out.println();
 					}
@@ -102,8 +80,7 @@ public class SocialHubDriver {
 				} else {
 					System.out.println("Please enter your content below:");
 					String content = in.nextLine();
-					Post p = new Post(u.getUsername(), content);
-					pServ.addPost(p);
+					pServ.addPost(u.getId(), u.getId(), content);
 					System.out.println("Post was received, are you finished? Press 1 for yes, press 2 for no");
 					choice = Integer.parseInt(in.nextLine());
 					done = (choice == 1) ? true : false;
@@ -113,7 +90,7 @@ public class SocialHubDriver {
 		
 		System.out.println("Goodbye :)");
 		in.close();
-		*/
+		
 	}
 	
 }
--- SocialHubWeek2/src/test/java/com/example/userTests/UserServiceTest.java
+++ SocialHubWeek2/src/test/java/com/example/userTests/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.userTests;
 
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -10,62 +11,64 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import com.example.dao.FileIO;
+import com.example.dao.UserDao;
+import com.example.exceptions.InvalidCredentialsException;
 import com.example.exceptions.UserDoesNotExistException;
 import com.example.models.User;
 import com.example.services.UserService;
 
 public class UserServiceTest {
 
-	private User testUser1 = new User("Test", "User", "testPass");
-	private User testUser2 = new User("Test", "User", "password");
-	private UserService uServ = new UserService("test.txt");
-	private FileIO<User> io = new FileIO<User>("test.txt");
+	@InjectMocks
+	public UserService uServ;
+	
+	@Mock
+	public UserDao uDao;
 	
 	@Before
-	public void clearTestFile() {
-		if(Files.exists(Paths.get("test.txt"))) {
-			System.out.println("Clearing the test file");
-			try {
-				Files.delete(Paths.get("test.txt"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
-	public void testEmptyUserList() {
-		List<User> userList = uServ.getAllUsers();
-		assertEquals("The list should have no users", 0, userList.size());
+	public void testValidLogin() {
+		User u1 = new User(1, "test", "user", "testuser", "test@email.com", "testpass");
+		User not = new User(0, "test", "user", "testuser", "test@mail.com", "testpass");
+		
+		when(uDao.getUserByUsername(anyString())).thenReturn(u1);
+		
+		User loggedIn = uServ.signIn("testuser", "testpass");
+		
+		assertEquals(u1.getId(), loggedIn.getId());
 	}
 	
-	@Test
-	public void testCreatNewUser() {
-		User rick = uServ.signUp("Rick", "Sanchez", "ilovemorty");
-		List<User> userList = uServ.getAllUsers();
+	@Test(expected = UserDoesNotExistException.class)
+	public void testInvalidUsername() {
+		User u1 = new User(1, "test", "user", "testuser", "test@email.com", "testpass");
+		User not = new User(0, "test", "user", "testuser", "test@mail.com", "testpass");
 		
-		assertNotNull(rick);
-		assertEquals(rick.getUsername(), userList.get(0).getUsername());
-		assertEquals(1, userList.size());
+		when(uDao.getUserByUsername(anyString())).thenReturn(not);
+		
+		User loggedIn = uServ.signIn("testuser", "testpass");
 	}
 	
-	@Test
-	public void testCreateMultipleUsers() {
-		User morty = uServ.signUp("Morty", "Smith", "ihaterick");
-		User rick = uServ.signUp("Rick", "Sanchez", "ilovemorty");
+	@Test(expected = InvalidCredentialsException.class)
+	public void testInvalidPassword() {
+		User u1 = new User(1, "test", "user", "testuser", "test@email.com", "testpass");
+		User not = new User(1, "test", "user", "testuser", "test@mail.com", "wrongpass");
 		
-		List<User> userList = uServ.getAllUsers();
+		when(uDao.getUserByUsername(anyString())).thenReturn(not);
 		
-		assertNotNull(morty);
-		assertNotNull(rick);
-		assertEquals(2, userList.size());
-	}
-	
-	@Test(expected=UserDoesNotExistException.class)
-	public void testUserDoesNotExist() {
-		User rick = uServ.login("Rick", "ilovemorty");
+		uServ.signIn("testuser", "testpass");
 	}
 	
 }
