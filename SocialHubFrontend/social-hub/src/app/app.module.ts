e64e4ec6abcc4118b63613af23b45535c28eb397
elmcgill <ethan.mcgill2016@gmail.com>
2021-08-04 11:44:59 -0500

    start of socialhub frontend

---
+++ SocialHubFrontend/social-hub/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
